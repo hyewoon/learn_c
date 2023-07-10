13c556eec6f2b85ca2f51975c8fac27e0e98c98a
hyewoon <likethebegin@naver.com>
2023-07-10 22:39:19 +0900

    init commit

---
+++ app/src/main/java/com/example/revandviewbinding/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.example.revandviewbinding
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class MainActivity : AppCompatActivity() {
+
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val array = ArrayList <String>()
+
+        array.add("a")
+        array.add("b")
+        array.add("c")
+        array.add("a")
+        array.add("b")
+        array.add("c")
+        array.add("a")
+        array.add("b")
+        array.add("c")
+        array.add("a")
+        array.add("b")
+        array.add("c")
+        array.add("a")
+        array.add("b")
+        array.add("c")
+        array.add("a")
+        array.add("b")
+        array.add("c")
+        array.add("a")
+        array.add("b")
+        array.add("c")
+
+        val customAdapter = CustomAdapter(array)
+
+        val rv = findViewById<RecyclerView>(R.id.rv)
+        rv.adapter = customAdapter
+        rv.layoutManager = LinearLayoutManager(this)
+    }
+}
